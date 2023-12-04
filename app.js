3fc322dfa530778a73c4f7d90c88577278e77e27
corinahincu <hincucorina97@gmail.com>
2023-12-04 23:58:08 +0200

    conditional rendering

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-const app = Vue.createApp({
-  // root component
-  // data, fucntions 
- /* template: '<h2> I am the template </h2>' */
-  
+const app = Vue.createApp({  
   data() {
     return{
+      showBooks: true,
       title: 'The Final Empire',
       author: 'Brandon Sanderson',
       age: 45
     }
   },
   methods: {
-    changeTitle(title) {
-      /* console.log('you clicked me') */
-      /* this.title = 'Words of Randiance' */
-      this.title = title
+    toggleShowBooks(){
+      this.showBooks = !this.showBooks
     }
   }
 })
